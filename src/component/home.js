b117529fd86ab51692908bae26369b2e763f03a4
Nina Yang <ssssrxdd@gmail.com>
2020-05-29 12:18:11 -0400

    frontend fixing

--- src/component/home.js
+++ src/component/home.js
@@ -3,6 +3,13 @@ import FormData from 'form-data';
 import axios from 'axios';
 
 class Home extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            img: null,
+        }
+    }
+
 
     selectFile = (e) => {
         const url = URL.createObjectURL(e.target.files[0]);
@@ -23,12 +30,30 @@ class Home extends React.Component {
             .catch(err => console.log(err));
     }
 
+    getAllFiles = () => {
+        axios.get(`http://localhost:5000/file`)
+            .then(res => { 
+                const buffer =res.data[0].file.data.data;
+                console.log(buffer);
+                const bytes = [].slice.call(new Uint8Array(buffer));
+                let binary = '';
+                bytes.forEach((b) => binary += String.fromCharCode(b));
+                const imageStr = window.btoa(binary);
+                const base64Flag = 'data:image/jpg;base64,';
+                const image = base64Flag + imageStr;
+                this.setState({img: image});
+             })
+            .catch(err => console.log(err));
+    }
 
     render() {
         return (
 
             <div>
-                <input type='file' className='input' accept=".md, .jpg" onChange={this.selectFile}/>
+                <h1>Choose you file</h1>
+                <input type='file' className='input' accept=".md, .jpg" onChange={this.selectFile} />
+                <button onClick={this.getAllFiles}>click get file</button>
+                <img src={this.state.img}/>
             </div>
         );
     }
